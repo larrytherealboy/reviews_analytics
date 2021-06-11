e0ef4cc3554dfa9118ddb498492f8de097164bc6
Larry Chen <abclarryabclarry@gmail.com>
2021-06-11 19:22:01 +0800

    add filtering

--- read.py
+++ read.py
@@ -14,3 +14,12 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('每筆留言平均長度為', sum_len / len(data))
+
+# data 中的資料一筆一筆拿出來，每一筆資料為 d
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+
